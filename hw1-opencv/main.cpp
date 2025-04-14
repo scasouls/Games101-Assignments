41e1aeba3b6721cd855ab55bb2595a1d81bc44f9
scasouls <sanhun430@outlook.com>
2025-04-14 23:10:41 +0800

    第一次提交(测试使用)

---
+++ hw1-opencv/main.cpp
@@ -0,0 +1,184 @@
+#include "Triangle.hpp"
+#include "rasterizer.hpp"
+#include <Eigen/Eigen>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+constexpr double MY_PI = 3.1415926;
+
+Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
+{
+    Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
+
+    Eigen::Matrix4f translate;
+    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
+        -eye_pos[2], 0, 0, 0, 1;
+
+    view = translate * view;
+
+    return view;
+}
+
+Eigen::Matrix4f get_model_matrix(float rotation_angle)
+{
+    Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
+
+    // TODO: Implement this function
+    // Create the model matrix for rotating the triangle around the Z axis.
+    // Then return it.
+    float a = rotation_angle / 180.0 * MY_PI;
+    Eigen::Matrix4f transmodelZ;
+    transmodelZ <<
+        cos(a), -sin(a), 0, 0,
+        sin(a), cos(a), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
+
+    model = transmodelZ * model;
+
+    return model;
+}
+
+Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
+    float zNear, float zFar)
+{
+    // Students will implement this function
+
+    Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
+
+    // TODO: Implement this function
+    // Create the projection matrix for the given parameters.
+    // Then return it.
+    float angle = eye_fov / 180.0 * MY_PI;
+    float t = tan(angle / 2.0) * zNear;
+    float r = aspect_ratio * t;
+    float b = -t, l = -r;
+
+    Eigen::Matrix4f move, projective, MT;
+    move <<
+        2.0 / (r - l), 0.0, 0.0, (-r + l) / 2.0,
+        0.0, 2.0 / (t - b), 0.0, (-t + b) / 2.0,
+        0.0, 0.0, 2.0 / (zNear - zFar), (-zNear + zFar) / 2.0,
+        0.0, 0.0, 0.0, 1.0;
+
+    projective <<
+        zNear, 0, 0, 0,
+        0, zNear, 0, 0,
+        0, 0, zNear + zFar, -zNear * zFar,
+        0, 0, 1, 0;
+
+    MT <<
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, -1, 0,
+        0, 0, 0, 1;
+
+
+    projection = move * projective * MT * projection;
+
+    return projection;
+}
+
+Eigen::Matrix4f get_rotation(Vector3f axis, float angle) {
+    Eigen::Matrix4f rotation = Eigen::Matrix4f::Identity();
+
+    Eigen::Matrix3f I = Eigen::Matrix3f::Identity();
+
+    axis.normalize();
+
+    Eigen::Matrix3f N;
+    N <<
+        0, -axis(2), axis(1),
+        axis(2), 0, -axis(0),
+        -axis(1), axis(0), 0;
+
+    float a = angle / 180.0 * MY_PI;
+    Eigen::Matrix3f Rodrigues =
+        cos(a) * I +
+        (1 - cos(a)) * axis * axis.transpose() +
+        sin(a) * N;
+
+    
+    rotation.block<3, 3>(0,0) = Rodrigues;
+
+    return rotation;
+}
+
+int main(int argc, const char** argv)
+{
+    float angle = 0;
+    bool command_line = false;
+    std::string filename = "output.png";
+
+    if (argc >= 3) {
+        command_line = true;
+        angle = std::stof(argv[2]); // -r by default
+        if (argc == 4) {
+            filename = std::string(argv[3]);
+        }
+        else
+            return 0;
+    }
+
+    rst::rasterizer r(700, 700);
+
+    Eigen::Vector3f eye_pos = { 0, 0, 5 };
+
+    Eigen::Vector3f rotate_axis = { 1,1,0 };
+
+    std::vector<Eigen::Vector3f> pos{ {2, 0, -2}, {0, 2, -2}, {-2, 0, -2} };
+
+    std::vector<Eigen::Vector3i> ind{ {0, 1, 2} };
+
+    auto pos_id = r.load_positions(pos);
+    auto ind_id = r.load_indices(ind);
+
+    int key = 0;
+    int frame_count = 0;
+
+    if (command_line) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        //r.set_model(get_model_matrix(angle));
+        r.set_model(get_rotation(rotate_axis, angle));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+
+        cv::imwrite(filename, image);
+
+        return 0;
+    }
+
+    while (key != 27) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        //r.set_model(get_model_matrix(angle));
+        //Œß»∆»Œ“‚÷·–˝◊™
+        r.set_model(get_rotation(rotate_axis, angle));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+        cv::imshow("image", image);
+        key = cv::waitKey(10);
+
+        std::cout << "frame count: " << frame_count++ << '\n';
+
+        if (key == 'a') {
+            angle += 10;
+        }
+        else if (key == 'd') {
+            angle -= 10;
+        }
+    }
+
+    return 0;
+}
+
---
+++ hw2-opencv/rasterizer.hpp
@@ -0,0 +1,108 @@
+//
+// Created by goksu on 4/6/19.
+//
+
+#pragma once
+
+#include <Eigen/Eigen>
+#include <algorithm>
+#include "global.hpp"
+#include "Triangle.hpp"
+using namespace Eigen;//使用Eigen的命名空间
+
+//命名空间rst
+namespace rst
+{
+    enum class Buffers//枚举类
+    {
+        Color = 1,
+        Depth = 2
+    };
+
+    inline Buffers operator|(Buffers a, Buffers b)
+    {
+        return Buffers((int)a | (int)b);
+    }
+
+    inline Buffers operator&(Buffers a, Buffers b)
+    {
+        return Buffers((int)a & (int)b);
+    }
+
+    enum class Primitive
+    {
+        Line,
+        Triangle
+    };
+
+    /*
+     * For the curious : The draw function takes two buffer id's as its arguments. These two structs
+     * make sure that if you mix up with their orders, the compiler won't compile it.
+     * Aka : Type safety
+     * */
+    struct pos_buf_id
+    {
+        int pos_id = 0;
+    };
+
+    struct ind_buf_id
+    {
+        int ind_id = 0;
+    };
+
+    struct col_buf_id
+    {
+        int col_id = 0;
+    };
+
+    class rasterizer
+    {
+    public:
+        rasterizer(int w, int h);//初始化rasterizer，指定图像的分辨率（宽和高）
+
+        //将数据存储到当前帧中
+        pos_buf_id load_positions(const std::vector<Eigen::Vector3f>& positions);
+        ind_buf_id load_indices(const std::vector<Eigen::Vector3i>& indices);
+        col_buf_id load_colors(const std::vector<Eigen::Vector3f>& colors);
+
+        void set_model(const Eigen::Matrix4f& m);
+        void set_view(const Eigen::Matrix4f& v);
+        void set_projection(const Eigen::Matrix4f& p);
+
+        void set_pixel(const Eigen::Vector3f& point, const Eigen::Vector3f& color);//设置像素点颜色
+
+        void clear(Buffers buff);//清除帧缓存和深度缓存的数据
+
+        void draw(pos_buf_id pos_buffer, ind_buf_id ind_buffer, col_buf_id col_buffer, Primitive type);
+
+        std::vector<Eigen::Vector3f>& frame_buffer() { return frame_buf; }
+
+    private:
+        void draw_line(Eigen::Vector3f begin, Eigen::Vector3f end);
+
+        void rasterize_triangle(const Triangle& t);
+
+        // VERTEX SHADER -> MVP -> Clipping -> /.W -> VIEWPORT -> DRAWLINE/DRAWTRI -> FRAGSHADER
+
+    private:
+        Eigen::Matrix4f model;
+        Eigen::Matrix4f view;
+        Eigen::Matrix4f projection;
+
+        //存储各个帧的数据
+        std::map<int, std::vector<Eigen::Vector3f>> pos_buf;
+        std::map<int, std::vector<Eigen::Vector3i>> ind_buf;
+        std::map<int, std::vector<Eigen::Vector3f>> col_buf;
+
+        std::vector<Eigen::Vector3f> frame_buf;
+
+        std::vector<float> depth_buf;
+        int get_index(int x, int y);
+
+        int width, height;
+
+        //给不同帧分配id
+        int next_id = 0;
+        int get_next_id() { return next_id++; }
+    };
+}
